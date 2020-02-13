5e1a4bed99a719192f6a38ef52f47266d6b18631
Jared Haight <1220886+jaredhaight@users.noreply.github.com>
2020-02-12 19:27:22 -0800

    updates

--- WindowsAttackAndDefenseLab/DSC/DomainConfig/DomainConfig.ps1
+++ WindowsAttackAndDefenseLab/DSC/DomainConfig/DomainConfig.ps1
@@ -136,12 +136,12 @@
             New-GPO -Name "Shared Folder"
             Import-GPO -Path "C:\WAAD\GPOs" -BackupId '{FF68FA65-A8D6-448D-87E5-6140373380CF}' -TargetName "Disable Firewall"
             Import-GPO -Path "C:\WAAD\GPOs" -BackupId '{BD3497A3-0BBC-4F59-8B26-F54C6CA6FD07}' -TargetName "Shared Folder"
-            Import-GPO -Path "C:\WAAD\GPOs" -BackupId '{AC5D004D-2C93-46AB-A1F8-2D6A64CF491F}' -TargetName "WAAD Default"
+            Import-GPO -Path "C:\WAAD\GPOs" -BackupId '{53C2ABBF-3752-415A-971B-F6E8A8BF6AF7}' -TargetName "WAAD Default"
             Import-GPO -Path "C:\WAAD\GPOs" -BackupId '{9FF1FF6F-FB61-4961-A30B-77148F45B36B}'-TargetName "Student Computers"
             New-GPLink -Name "Disable Firewall" -Target "OU=Domain Controllers,DC=ad,DC=waad,DC=training"
             New-GPLink -Name "Disable Firewall" -Target "OU=Production,DC=AD,DC=WAAD,DC=TRAINING"
             New-GPLink -Name "Shared Folder" -Target "OU=Production,DC=AD,DC=WAAD,DC=TRAINING"
-            New-GPLink -Name "WAAD Default" -Target "DC=AD,DC=WAAD,DC=TRAINING"
+            New-GPLink -Name "WAAD Default" -Target "DC=AD,DC=WAAD,DC=TRAINING" -Order 1
             New-GPLink -Name "Student Computers" -Target "OU=Computers,OU=Class,DC=AD,DC=WAAD,DC=TRAINING"
           }
           Catch {
@@ -151,7 +151,15 @@
           }
         }
         GetScript =  { @{} }
-        TestScript = { try {Get-GPO -Name "WAAD Default2" -ErrorAction Stop | Out-null; return $true} catch { $false } }
+        TestScript = { 
+          try {
+            Get-GPO -Name "WAAD Default" -ErrorAction Stop | Out-null; 
+            return $true
+          } 
+          catch { 
+            return $false 
+          } 
+        }
         DependsOn = "[Archive]UnzipClassFiles","[xADOrganizationalUnit]ProductionServersOU","[xADOrganizationalUnit]ClassComputersOU"
     }
 
